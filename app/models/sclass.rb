59cd251f56556b9475e3742385f042756b4bc95c
kishan <kishaniitd@gmail.com>
2015-06-13 18:39:37 +0530

    procfile updated

--- app/models/sclass.rb
+++ app/models/sclass.rb
@@ -3,7 +3,7 @@ class Sclass < ActiveRecord::Base
   has_many:admissions
   
   def self.options_for_select
-  order('LOWER(id)').map { |e| [e.sclassname, e.id] }
+  order('id').map { |e| [e.sclassname, e.id] }
   end
 
 end
